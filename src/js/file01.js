bded3303958a64180da11a37918d1b8fa190b25d
Samuel Meza <81500115+Leaxmus@users.noreply.github.com>
2025-10-27 19:53:09 +0000

    feat: agregar navegación y sección de productos a la página de inicio

--- src/js/file01.js
+++ src/js/file01.js
@@ -1,4 +1,5 @@
 "use strict";
+import { fetchProducts } from './functions.js';
 
 const showToast = () => {
     const toast = document.getElementById("toast-interactive");
@@ -17,7 +18,52 @@ const showVideo = () => {
     }
 };
 
+let renderProducts = () => {
+    fetchProducts("https://data-dawm.github.io/datum/reseller/products.json")
+        .then(response => {
+            if (response.success) {
+                let container = document.getElementById("products-container");
+                container.innerHTML = "";
+
+                let products = response.body.slice(0, 6);
+
+                products.forEach(product => {
+                    let productHTML = `
+                        <div class="space-y-4 bg-white dark:bg-gray-800 p-4 rounded-2xl shadow">
+                            <img
+                                class="w-full h-40 bg-gray-300 dark:bg-gray-700 rounded-lg object-cover transition-transform duration-300 hover:scale-[1.03]"
+                                src="[PRODUCT.IMGURL]" alt="[PRODUCT.TITLE]">
+                            <h3
+                                class="h-6 text-xl font-semibold tracking-tight text-gray-900 dark:text-white hover:text-black-600 dark:hover:text-white-400">
+                                $[PRODUCT.PRICE]
+                            </h3>
+
+                            <div class="h-5 rounded w-full">[PRODUCT.TITLE]</div>
+                                <div class="space-y-2">
+                                    <a href="[PRODUCT.PRODUCTURL]" target="_blank" rel="noopener noreferrer"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full inline-block">
+                                        Ver en Amazon
+                                    </a>
+                                    <div class="hidden"><span class="1">[PRODUCT.CATEGORY_ID]</span></div>
+                                </div>
+                            </div>
+                        </div>`;
+
+                    productHTML = productHTML.replaceAll("[PRODUCT.TITLE]", product.title.length > 20 ? product.title.substring(0, 20) + "..." : product.title);
+                    productHTML = productHTML.replaceAll('[PRODUCT.CATEGORY_ID]', product.category_id);
+                    productHTML = productHTML.replaceAll("[PRODUCT.IMGURL]", product.imgurl);
+                    productHTML = productHTML.replaceAll("[PRODUCT.PRICE]", product.price.toFixed(2));
+
+                    container.innerHTML += productHTML;
+                });
+            } else {
+                alert(response.body);
+            }
+        });
+}
+
 (() => {
     showToast();
     showVideo();
+    renderProducts();
 })();
